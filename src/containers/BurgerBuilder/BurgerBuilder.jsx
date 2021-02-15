a18bee39eb6728051194ac39655686f6fa764c8f
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-02-15 16:39:05 +0330

    Feat: ingredients state applied

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -3,6 +3,14 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Auxiliary';
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 1,
+			meat: 1,
+			cheese: 2,
+			bacon: 1
+		}
+	};
 	render() {
 		return (
 			<Aux>
