296a1eeed03c27a7491e7cb120ea060084385d6e
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-02-16 19:03:05 +0330

    Feat: will show custom message if ingredients array was empty

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -5,12 +5,19 @@ const burger = props => {
 	// props
 	const { ingredients } = props;
 
-	const transformedIngredients = Object.keys(ingredients).map(igKey => {
-		return [...Array(ingredients[igKey])].map((_, index) => {
-			return <BurgerIngredient key={igKey + index} type={igKey} />;
-		});
-	});
-
+	let transformedIngredients = Object.keys(ingredients)
+		.map(igKey => {
+			return [...Array(ingredients[igKey])].map((_, index) => {
+				return <BurgerIngredient key={igKey + index} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+		
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Pls start adding ingredients !</p>;
+	}
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type='bread-top' />
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -5,10 +5,10 @@ import Aux from '../../hoc/Auxiliary';
 class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
-			salad: 1,
-			meat: 1,
-			cheese: 2,
-			bacon: 1
+			salad: 0,
+			meat: 0,
+			cheese: 0,
+			bacon: 0
 		}
 	};
 	render() {
