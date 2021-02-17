4318cd71a9dac777d60d0b53a514b31bef49b9c9
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-02-17 20:08:46 +0330

    Feat: render buildControls dynamically

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,7 +1,18 @@
-const buildControls = props => (
-  <div>
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.module.css'
+const controls = [
+	{ label: 'Cheese', type: 'cheese' },
+	{ label: 'Salad', type: 'salad' },
+	{ label: 'Bacon', type: 'bacon' },
+	{ label: 'Meat', type: 'meat' }
+];
 
-  </div>
+const buildControls = props => (
+	<div className={classes.BuildControls}>
+		{controls.map(control => (
+			<BuildControl label={control.label} type={control.type} />
+		))}
+	</div>
 );
 
 export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Auxiliary';
 
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
@@ -15,7 +16,7 @@ class BurgerBuilder extends Component {
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<div>Build Controls</div>
+				<BuildControls />
 			</Aux>
 		);
 	}
