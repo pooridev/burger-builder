ebc556d2eb6edcb3ef8b6a8e554527fec3ad1d6f
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-06-30 00:28:55 +0430

    Feat: add redux devtools setup

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
