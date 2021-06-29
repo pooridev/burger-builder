bfa13bcc2d16b4a45ba6f693ad134b064bfef114
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-06-30 00:22:40 +0430

    Style: add tiny styles to Order

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,18 +1,41 @@
+import React from 'react';
+
 import classes from './Order.module.css';
 
-const order = props => (
-  <div className={classes.Order}>
-    <p>
-      Ingredients: 
-      salad ({props.ingredients.salad})
-      meat ({props.ingredients.meat}) 
-      bacon ({props.ingredients.bacon}) 
-      cheese ({props.ingredients.cheese})
-    </p>
-    <p>
-      Price: <strong>USD {Number(props.price).toFixed(2)}</strong>
-    </p>
-  </div>
-);
+const Order = props => {
+  const ingredients = [];
 
-export default order;
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }}
+        key={ig.name}>
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default Order;
