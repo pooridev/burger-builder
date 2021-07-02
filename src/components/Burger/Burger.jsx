c25f1f6e5b5ff11edd10d54ac472e72c9b95eba5
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-07-03 00:57:14 +0430

    Style: change scrollbar style & make some minor changes

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -2,34 +2,35 @@ import React from 'react';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import PropTypes from 'prop-types';
 import classes from './Burger.module.css';
+
 const burger = props => {
-	// props
-	const { ingredients } = props;
+  // props
+  const { ingredients } = props;
 
-	let transformedIngredients = Object.keys(ingredients)
-		.map(igKey => {
-			return [...Array(ingredients[igKey])].map((_, index) => {
-				return <BurgerIngredient key={igKey + index} type={igKey} />;
-			});
-		})
-		.reduce((arr, el) => {
-			return arr.concat(el);
-		}, []);
+  let transformedIngredients = Object.keys(ingredients)
+    .map(igKey => {
+      return [...Array(ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
-	if (transformedIngredients.length === 0) {
-		transformedIngredients = <p>Pls start adding ingredients !</p>;
-	}
-	return (
-		<div className={classes.Burger}>
-			<BurgerIngredient type='bread-top' />
-			{transformedIngredients}
-			<BurgerIngredient type='bread-bottom' />
-		</div>
-	);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Pls start adding ingredients !</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type='bread-top' />
+      {transformedIngredients}
+      <BurgerIngredient type='bread-bottom' />
+    </div>
+  );
 };
 
 burger.propTypes = {
-	ingredients: PropTypes.object
+  ingredients: PropTypes.object
 };
 
 export default burger;
